85b5f75867854b2dfc70022389211acddb621d72
Rodrigo Iraola <rodrigoiraola@macbook-air-3.myfiosgateway.com>
2023-03-26 22:25:32 -0400

    week-14

---
+++ src/data.js
@@ -0,0 +1,56 @@
+const movies = [
+  {
+    id: 1,
+    title: "Avengers ENDGAME",
+    poster: 'https://cdn.marvel.com/content/2x/MLou2_Payoff_1-Sht_Online_DOM_v7_Sm.jpg',
+    synopsis:
+      "After the devastating events of Avengers: Infinity War (2018), the universe is in ruins. With the help of remaining allies, the Avengers assemble once more in order to reverse Thanos' actions and restore balance to the universe",
+    score: 10,
+    reviews: [
+      {
+        id: 1,
+        author: "MarveLov3r12",
+        title: `ONE OF THE BEST MARVEL MOVIES`,
+        content: `One of the best movies marvel movies! packed with action, drama, and comedy. Really well balanced, one bad thing is the characters that do not make it.`,
+        rating: 10,
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Avatar, The Way of the Water",
+    poster: 'https://m.media-amazon.com/images/M/MV5BYjhiNjBlODctY2ZiOC00YjVlLWFlNzAtNTVhNzM1YjI1NzMxXkEyXkFqcGdeQXVyMjQxNTE1MDA@._V1_FMjpg_UX1000_.jpg',
+    synopsis:
+      "Jake Sully and Ney'tiri have formed a family and are doing everything to stay together. However, they must leave their home and explore the regions of Pandora. When an ancient threat resurfaces, Jake must fight a difficult war against the humans.",
+    
+    score: 10,
+    reviews: [
+      {
+        id: 1,
+        author: "anonymous345",
+        title: "ENGAGING",
+        content: `It’s more than you can possibly imagine. A fantasy world so vivid and real it commands your full attention for 3h 12m. And when it’s done, you’ll still want to linger in this heightened state of make believe.`,
+        rating: 10,
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Harry Potter and the Deadly Hallows Pt 2",
+    poster: 'https://i.ebayimg.com/images/g/Fe4AAOSw~p5jLTGm/s-l1600.jpg',
+    synopsis:
+      "A clash between good and evil awaits as young Harry. The friends must search for the Horcruxes that keep the dastardly wizard immortal. Harry and Voldemort meet at Hogwarts Castle for an epic showdown.",
+    score: 9,
+    reviews: [
+      {
+        id: 1,
+        author: "Potterf@n37",
+        title: `Conclusion to an Amazong Saga!`,
+        content: `For 10 years and eight films we've gotten to watch these young actors grow as people, as performers and as characters. It has been a true coming-of-age saga, in the fullest and most moving sense of the term`,
+        rating: 9,
+      },
+    ],
+  },
+];
+
+export default movies
